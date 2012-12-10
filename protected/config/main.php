41f24648343b0e0d3c512687a0ebb9cd50cebfc2
Flavia Barreto <flavia.t.barreto@gmail.com>
2012-12-09 22:27:29 -0300

    Icone
    Detalhe do Icone

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Agenda Telefônica',
+	'name'=>'Flagenda',
 	'defaultController' => 'phonebook/index',
 
 	// preloading 'log' component
--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -23,7 +23,7 @@
 <div class="container" id="page">
 
 	<div id="header">
-		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
+		<div id="logo"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/phonebook1.png"/><?php echo CHtml::encode(Yii::app()->name); ?></div>
 	</div><!-- header -->
 
 	<div id="mainmenu">
